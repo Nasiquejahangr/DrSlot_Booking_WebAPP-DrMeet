4ba3692c00c978b5b29460195c33c1a6a8e5610a
md nasique <nasiquejahangir000@gmail.com>
2026-02-16 16:28:10 +0530

    Update frontend files

--- Frontend/src/components/Logoandprofile.jsx
+++ Frontend/src/components/Logoandprofile.jsx
@@ -12,13 +12,14 @@ function Logoandprofile() {
              border-gray-300 p-6 fixed top-0 left-0 w-full
               bg-white z-50"
                 onClick={() => navigate('/')}>
-                <div className="h-13 w-13">
+                <div className="h-14 w-14 rounded-full overflow-hidden bg-white shadow-sm border border-gray-200 flex items-center justify-center">
                     <img src={img} alt="Logo" className="w-full h-full object-cover" />
                 </div>
                 <div
-                    className='h-9 w-20 flex items-center justify-center cursor-pointer'
-                    onClick={() => navigate('/profile')}>
-                    <div className='h-11 w-11 rounded-full bg-gray-200 flex items-center justify-center'>
+                    className='h-9 w-20 flex items-center justify-center cursor-pointer'>
+
+                    <div className='h-11 w-11 rounded-full bg-gray-200 flex items-center justify-center cursor-pointer'
+                        onClick={() => navigate('/Profile')}>
                         <CiUser size={24} className="text-gray-700" />
 
                     </div>
